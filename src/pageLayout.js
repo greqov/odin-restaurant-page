db164fed29a93def50f46ba524571f8902a8f21e
Ivan Greqov <idgreq@bk.ru>
2022-03-07 19:04:26 +0300

    Add links to header nav

--- src/pageLayout.js
+++ src/pageLayout.js
@@ -1,3 +1,36 @@
+// TODO: move navigation to own module?
+function headerNav() {
+  const nav = document.createElement('nav');
+
+  const links = [
+    {
+      title: 'Home',
+      href: '#home',
+    },
+    {
+      title: 'Menu',
+      href: '#menu',
+    },
+    {
+      title: 'Contacts',
+      href: '#contacts',
+    },
+  ];
+
+  let navListItems = '';
+
+  links.forEach(({ title, href }) => {
+    navListItems += `
+    <li class="header-nav__item">
+      <a href="${href}" class="header-nav__link">${title}</a>
+    </li>`;
+  });
+
+  nav.innerHTML = `<ul class="header-nav__list">${navListItems}</ul>`;
+
+  return nav;
+}
+
 function pageLayout() {
   const pageContainer = document.createElement('div');
   pageContainer.classList.add('container');
@@ -8,9 +41,9 @@ function pageLayout() {
   const logo = document.createElement('a');
   logo.textContent = 'Logo';
   header.appendChild(logo);
+  // TODO: add image to logo
 
-  const nav = document.createElement('nav');
-  nav.textContent = 'header nav';
+  const nav = headerNav();
   // TODO: use data from JSON?
 
   header.appendChild(nav);
@@ -18,10 +51,10 @@ function pageLayout() {
   const footer = document.createElement('footer');
   footer.classList.add('page-footer');
   footer.textContent = 'footer info';
+  // TODO: add link to github (with svg icon?)
 
   const main = document.createElement('main');
   main.classList.add('page-main');
-  main.textContent = 'main content here';
 
   [header, main, footer].forEach((el) => pageContainer.appendChild(el));
 
